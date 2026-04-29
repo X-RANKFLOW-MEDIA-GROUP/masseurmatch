d1d9522093fd5175c91b558cab57009332220245
GLEICIMAR HALL <segatti.hall@gmail.com>
2026-04-29 19:32:02 -0400

    Enhance cn function with JSDoc comment
    Added documentation for the cn utility function.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-﻿import { clsx, type ClassValue } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Utility function to merge tailwind classes safely.
+ * Solves class conflicts when using Tailwind CSS and clsx.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
