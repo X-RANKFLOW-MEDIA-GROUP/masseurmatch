e4e1c78406e5d756dc88a952ff1b6ae6211f01d7
GLEICIMAR HALL <segatti.hall@gmail.com>
2026-05-02 11:00:19 -0400

    Restore legacy utility script entrypoints

---
+++ scripts/_test-login.mjs
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log('[deprecated] scripts/_test-login.mjs was consolidated.');
+console.log('Use: node scripts/test-api-routes.mjs');
---
+++ scripts/_test-login2.mjs
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log('[deprecated] scripts/_test-login2.mjs was consolidated.');
+console.log('Use: node scripts/test-api-routes.mjs');
