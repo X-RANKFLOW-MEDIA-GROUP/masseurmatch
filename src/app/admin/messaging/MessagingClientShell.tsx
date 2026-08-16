122c783b81fd3d6bd16b31b1428935f3b4b75d3f
GLEICIMAR HALL <segatti.hall@gmail.com>
2026-08-16 17:31:40 -0500

    feat(admin): show all masseurs in messaging dashboard

--- src/app/admin/messaging/MessagingClientShell.tsx
+++ src/app/admin/messaging/MessagingClientShell.tsx
@@ -2,12 +2,14 @@
 
 import CsvContactsImport from "./CsvContactsImport";
 import ImessageBridgeHealth from "./ImessageBridgeHealth";
+import MasseursDirectory from "./MasseursDirectory";
 import AdminMessaging from "./AdminMessaging";
 
 export default function MessagingClientShell() {
   return (
     <div className="space-y-4">
       <ImessageBridgeHealth />
+      <MasseursDirectory />
       <CsvContactsImport onImported={() => window.location.reload()} />
       <AdminMessaging />
     </div>
